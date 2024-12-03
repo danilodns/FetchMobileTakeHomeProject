70361709718dad4f9af8626081ef6333e3877dae
Danilo Silveira <danilo.nobrega88@gmail.com>
2024-12-03 13:41:05 -0500

    Initial Commit

---
+++ FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Danilo Silveira on 2024-12-03.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
