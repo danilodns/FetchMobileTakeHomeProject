cca885be6fb2c1195311d9b778503a64cdb159c8
Danilo Silveira <danilo.nobrega88@gmail.com>
2024-12-03 13:45:59 -0500

    Created the Model of recipes

---
+++ FetchTakeHome/Model/Recipe.swift
@@ -0,0 +1,16 @@
+//
+//  Recipe.swift
+//  FetchTakeHome
+//
+//  Created by Danilo Silveira on 2024-12-03.
+//
+
+struct Recipe {
+    var uuid: String
+    var cuisine: String
+    var name: String
+    var photoUrlLarge: String?
+    var photoUrlSmall: String?
+    var sourceUrl: String?
+    var youtubeUrl: String?    
+}
